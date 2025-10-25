7a1caf470290d1da0e15c2d2c1fe7e9a17f6316b
Avazbek <telegram.vzlom@gmail.com>
2025-10-25 15:51:03 +0500

    Initial commit

---
+++ src/main/java/api/debt/book/DebtBookApplication.java
@@ -0,0 +1,13 @@
+package api.debt.book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DebtBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DebtBookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/api/debt/book/DebtBookApplicationTests.java
@@ -0,0 +1,13 @@
+package api.debt.book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DebtBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
